b2b49a022248608c23c4b2a42a3f60fb8d9deaa1
Mohammed Nayeem <mohammednayeem45@gmail.com>
2018-04-20 00:31:37 +0530

    Add custom html file & post.

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return render_template("index.html")
+    return render_template("cover.html")
 #    return "index.html"
 
 @app.route('/cover')
